548d6c7a5fa56d4e84ae85adfacbc5fc654a44bd
Hannah Catherine Trask <hannah@trynow.io>
2022-09-28 07:52:36 -0600

    [COM-233] Minor codestyle changes

--- src/tests/execute.spec.ts
+++ src/tests/execute.spec.ts
@@ -5,14 +5,16 @@ describe('MiddlewareRegistry.execute', () => {
     it('should not call a middleware before execute', async () => {
         const request = { url: '/api/a' } as NextApiRequest;
         const registry = new MiddlewareRegistry(request);
-        // GIVEN a middleware
+
+        // GIVEN a middleware and a route /api/a
         const middleware = jest.fn();
-        // GIVEN route /api/a
         registry.add('/api/(.*)', middleware, { transparent: true });
         registry.add('/api/a', middleware);
         expect(middleware).not.toHaveBeenCalled();
+
         // WHEN the registry is executed
         await registry.execute();
+
         // EXPECT the middleware to have been called
         expect(middleware).toHaveBeenCalledTimes(2);
     });
